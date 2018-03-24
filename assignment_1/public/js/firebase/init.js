787602a556cb5c535401abe9beaeba61133ec04d
VagnerSilva <vagner23silva@gmail.com>
2018-03-24 03:11:14 -0300

    remove configs

--- assignment_1/public/js/firebase/init.js
+++ assignment_1/public/js/firebase/init.js
@@ -2,12 +2,12 @@
 if (typeof firebase === 'undefined') 
     throw new Error('hosting/init-error: Firebase SDK not detected. You must include it before /__/firebase/init.js');
 const config = {
-    apiKey: "AIzaSyBYhmNd4YCoAXA7XTqa-jUWwzRniJiT8WI",
-    authDomain: "gama-assignment-1-d87a1.firebaseapp.com",
-    databaseURL: "https://gama-assignment-1-d87a1.firebaseio.com",
-    projectId: "gama-assignment-1-d87a1",
+    apiKey: "",
+    authDomain: "",
+    databaseURL: "",
+    projectId: "",
     storageBucket: "",
-    messagingSenderId: "1005402279629"
+    messagingSenderId: ""
   };
   firebase.initializeApp(config);
 
