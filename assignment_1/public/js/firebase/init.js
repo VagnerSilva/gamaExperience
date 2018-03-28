8fc25870f2a4e762a4ec8b407655287daaa172aa
VagnerSilva <vagner23silva@gmail.com>
2018-03-27 21:32:43 -0300

    reajustando firebase

--- assignment_1/public/js/firebase/init.js
+++ assignment_1/public/js/firebase/init.js
@@ -2,12 +2,12 @@
 if (typeof firebase === 'undefined') 
     throw new Error('hosting/init-error: Firebase SDK not detected. You must include it before /__/firebase/init.js');
     var config = {
-        apiKey: "AIzaSyBYhmNd4YCoAXA7XTqa-jUWwzRniJiT8WI",
-        authDomain: "gama-assignment-1-d87a1.firebaseapp.com",
-        databaseURL: "https://gama-assignment-1-d87a1.firebaseio.com",
-        projectId: "gama-assignment-1-d87a1",
-        storageBucket: "gama-assignment-1-d87a1.appspot.com",
-        messagingSenderId: "1005402279629"
+        apiKey: "",
+        authDomain: "",
+        databaseURL: "",
+        projectId: "",
+        storageBucket: "",
+        messagingSenderId: ""
       };
   firebase.initializeApp(config);
 
